74f7ed8bececdbc5efda01a20ea2e401ee72a616
EdgarHan <poseidon0288@gmail.com>
2018-01-19 19:21:24 +0900

    have to frog river

---
+++ 4CountingElements/FrogRiverOne.cpp
@@ -0,0 +1,39 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+#include <numeric> 
+
+
+using namespace std;
+
+int solution(int X, vector<int> &A);
+
+int main () {
+    std::vector<int> A = {2, 2, 2, 2, 2};
+    solution(2, A);
+}
+
+int solution(int X, vector<int> &A) {
+    bool step[X] = {false};
+    int cnt = 0;
+
+    for (unsigned int i = 0 ; i < A.size(); i++) {
+        if (A[i] == X) {
+            step[A[i]] = true;
+        }
+        ++cnt;
+    }
+
+    cout << cnt << endl;
+
+    for (unsigned int i = 0 ; i < X; i++) {
+        cout << (step[i] ? 1 : 0);
+    }
+
+    int sum = std::accumulate(step, step + X, 0);
+    if(sum == X)
+        return cnt;
+
+
+    return -1;
+}
