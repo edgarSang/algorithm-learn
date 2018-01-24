da564a4be74df0a8d08a8343fe03108f4c8241b0
EdgarHan <poseidon0288@gmail.com>
2018-01-24 20:42:36 +0900

    combine

--- 4CountingElements/FrogRiverOne.cpp
+++ 4CountingElements/FrogRiverOne.cpp
@@ -9,31 +9,38 @@ using namespace std;
 int solution(int X, vector<int> &A);
 
 int main () {
-    std::vector<int> A = {2, 2, 2, 2, 2};
-    solution(2, A);
+    std::vector<int> A = {1, 3, 1, 4, 2, 3, 5, 4};
+    solution(5, A);
 }
 
 int solution(int X, vector<int> &A) {
     bool step[X] = {false};
     int cnt = 0;
+    int sum = 0;
 
     for (unsigned int i = 0 ; i < A.size(); i++) {
-        if (A[i] == X) {
-            step[A[i]] = true;
+        if(A[i] <= X) {
+            step[A[i]-1] = true;
+            ++cnt;
         }
-        ++cnt;
+        
+        for (unsigned int i = 0 ; i < X; i++) {
+            cout << (step[i] ? 1 : 0);
+        }
+        cout << endl;
     }
-
-    cout << cnt << endl;
-
+    
+    sum = std::accumulate(step, step + X, 0);
+    if(sum == X) {
+        cout << cnt << endl;
+        return cnt-1;
+    }
+    
     for (unsigned int i = 0 ; i < X; i++) {
         cout << (step[i] ? 1 : 0);
     }
 
-    int sum = std::accumulate(step, step + X, 0);
-    if(sum == X)
-        return cnt;
-
+    cout << endl << cnt << endl;
 
     return -1;
 }
