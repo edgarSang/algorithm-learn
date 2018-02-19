6314843668166a8d96bbbeefb61179e1287787e5
EdgarHan <poseidon0288@gmail.com>
2018-02-19 23:19:51 +0900

    solve it

--- june/4dp/9095.cpp
+++ june/4dp/9095.cpp
@@ -1,26 +1,35 @@
 #include <iostream>
 
-#define MAX 11
+#define MAX 12
 
 using namespace std;
-int d[MAX] = {0}
+int d[MAX] = {0};
 
-int plus(int n) {
+int onePlus(int n) {
+    d[0] = 0;
+    d[1] = 1;
+    d[2] = 2;
+    d[3] = 4;
     
-    if(d[n] > 0) return d[n];
-
+    if(n < 4) return d[n];
+    if(d[n-1] == 0) {
+        onePlus(n-1);
+    }
+    d[n] = d[n-1] + d[n-2] + d[n-3];
+    
     return d[n];
 }
-
 
 int main() {
     int n,t;
 
+    cin >> t;
     for(int i = 1; i <= t ; i++){
         cin >> n;
-        plus(n);
-    }
+        int result = onePlus(n);
 
+        cout << result << '\n';
+    }
 
     return 0;
 }
