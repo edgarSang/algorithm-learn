c9f708d3a0aeff59ed0ea23a5540c57e1be7e9d4
EdgarHan <poseidon0288@gmail.com>
2018-03-22 23:55:19 +0900

    gogo have to

--- june/6dp-3/11054.cpp
+++ june/6dp-3/11054.cpp
@@ -8,31 +8,31 @@ inline T comp(T a, T b) {
 
 using namespace std;
 int A[MAX] = {0};
-int RA[MAX] = {0};
-int d[MAX] = {0};
+int d[MAX][2] = {0};
 int m = 0, rm = 0, mi = 0;
 
-int bio(int n, int A[]) {
-    int localMax = 0;
+int bi(int n, int A[]) {
+    int max = 0;
     for(int j=1; j<=n; j++){
         for(int i=0; i<j; i++){
-            d[j] = A[i] < A[j] ? comp(d[i]+1, d[j]) : comp(d[j], 1);
-            if(d[j] > localMax) {
-                localMax = d[j];
-                mi = j;
+            d[j][0] = A[i] < A[j] ? comp(d[i][0]+1, d[j][0]) : comp(d[j][0], 1);
+            // if(d[j][0] > localMax) {
+            //     localMax = d[j][0];
+            //     mi = j;
+            // }
+        }
+        for(int c=j+1; c<=n; c++){
+            d[c-1][1] = 1;
+            for(int b=j; b<c; b++) {
+                d[c][1] = A[b] > A[c] ? comp(d[b][1]+1, d[c][1]) : comp(d[c][1], 1);
             }
         }
-    }
-    for(int j=mi+1; j<=n; j++){
-        for(int i=mi; i<j; i++){
-            d[j] = A[i] > A[j] ? comp(d[i]+1, d[j]) : comp(d[j], 1);
-            if(d[j] > localMax) {
-                localMax = d[j];
-            }
-        }
+        for(int c=j; c<=n; c++){
+            cout << d[j][1] << ' ';
+        } cout << '\n';
     }
 
-    return localMax;
+    return max;
 }
 
 int main() {
@@ -42,17 +42,10 @@ int main() {
     for(int i=1; i<=n; i++){
         cin >> A[i];
     }
-    for(int i=n; i>0; i--){
-        RA[i] = A[cnt];
-        cnt++;
-    }
+
     
-    m = bio(n, A);
+    m = bi(n, A);
     mi = 0;
-    for(int i=1; i<=n; i++){
-        d[i] = 0;
-    }
-    rm = bio(n, RA);
 
     cout << comp(m, rm) << '\n';
     return 0;
