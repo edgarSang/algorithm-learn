8053f4f074bed7f8ca4e44549ca18bb8dad9c3df
EdgarHan <poseidon0288@gmail.com>
2018-03-25 23:57:30 +0900

    gggggggggggggg

---
+++ june/7dp-4/1699pass.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#define MAX 100001
+
+using namespace std;
+
+int d[MAX] = {0};
+
+int main() {
+    cin >> n;
+    for(int i=1; i<=n; i++) {
+        for(int j=1; j<=n; j++){
+
+        }
+    }
+
+}
