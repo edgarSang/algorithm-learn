dff26f22205e7aa1c1d0624a2acc487c6d4c5e5b
EdgarHan <poseidon0288@gmail.com>
2018-06-17 19:52:51 +0900

    graph 끝 문제풀어야함

--- june/12graph2/dfs.cpp
+++ june/12graph2/dfs.cpp
@@ -1,17 +1,58 @@
 #include <iostream>
 #include <vector>
+#include <stack>
+#include <queue>
 
 using namespace std;
 int N, M, V;
-int A[10][10] = {0,};
-int check[10] = {0,};
+int check[1001] = {0};
+vector<int> A[1001];
 
-void dfs(int v) {
+// void dfsMatrix(int v) {
+//     cout << v << " ";
+//     check[v] = 1;
+//     for(int i=1; i<=N; i++){
+//         if(arr[v][i] == 1 && !check[i])
+//             dfs(i);
+//     }
+// }
+
+void dfsList(int v) {
+    std::fill_n(check, M, 0);
+    stack<int> s;
+    s.push(v);
+
+    while(!s.empty()) {
+        int top = s.top();
+        s.pop();
+        
+        if(check[top]) continue;
+        check[top] = 1;
+        cout << top << " ";
+
+        for(int i=0; i<A[top].size(); i++) {
+            if(check[A[top][i]] == 0) {
+                s.push(A[top][i]);
+            }
+        }
+    }
+}
+
+void bfsList(int v) {
+    std::fill_n(check, M, 0);
+    queue<int> q;
     check[v] = 1;
-    cout << v << " ";
-    for(int i=1; i<=N; i++) {
-        if(A[v][i] == 1 && !check[i]) {
-            dfs(i);
+    q.push(v);
+
+    while(!q.empty()) {
+        int x = q.front();
+        q.pop();
+        cout << x << " ";
+        for(int i=0;i<A[x].size();i++) {
+            if(check[A[x][i]] == 0) {
+                check[A[x][i]] = 1;
+                q.push(A[x][i]);
+            }
         }
     }
 }
@@ -19,13 +60,16 @@ void dfs(int v) {
 int main() {
     cin >> N >> M >> V;
     
-    for(int i=1; i<=M; i++) {
-        int u, e;
+    for(int i=1;i<=M;i++) {
+        int u,e;
         cin >> u >> e;
-        A[u][e] = A[e][u] = 1;
+        A[u].push_back(e);
+        A[e].push_back(u);
     }
-    dfs(V);
-
+    dfsList(V);
+    cout << endl;
+    bfsList(V);
+    
     return 0;
 }
 
@@ -45,3 +89,24 @@ int main() {
     //     cin >> u >> e;
     //     A[u][e] = A[e][u] = 1;
     // } 
+
+
+        // stack<int> s;
+    // s.push(v);
+    
+    // while(!s.empty()) {
+    //     int top = s.top();
+    //     s.pop();
+        
+    //     if(check[top]) continue;
+
+    //     check[top] = 1;
+    //     cout << top << " ";
+
+    //     for(int i=0;i<A[top].size();i++) {
+    //         if(!check[A[top][i]]) {
+    //             s.push(A[top][i]);
+    //         }
+    //     }
+        
+    // }
