00edf16770aae6cd05294bcfda4b6f4b0cce223d
EdgarHan <poseidon0288@gmail.com>
2018-07-11 23:02:22 +0900

    TODO 이분그래프

--- june/13ConnectedComponent/1707.cpp
+++ june/13ConnectedComponent/1707.cpp
@@ -3,20 +3,32 @@
 #include <stack>
 #define MAX 20001
 
-int check[MAX] = {0,};
-vector<int> v[MAX];
 using namespace std;
 
-bool checkBi(int s) {
-    check[s] = 1;
+int check[MAX] = {0,};
+vector<int> A[MAX];
 
-    for(int i=0;i<=v[s].size();i++){
-        if(check[v[s][i]] == 1)
+bool checkBi(int s) {
+    if(check[s] == 2) {
+        if(!A[s].size()) {
+            return true;
+        } else {
             return false;
+        }
+    }
+    check[s] = 1;
+    
+    for(int i=0;i<A[s].size();i++){
+        if(check[A[s][i]] == 1) {
+            return false;
+        } else {
+            check[A[s][i]] = 2;
+        }
     }
     return true;
 }
-
+// TODO:  그래프 1-2 2-3 3-4 는 이분그래프인가?
+// 1-2 2-1은 이분그래프인가?
 int main() {
     int K,Vcnt,Ecnt;
     bool bi;
@@ -25,12 +37,18 @@ int main() {
     for(int j=1;j<=K;j++) {
         cin >> Vcnt >> Ecnt;
         bi = true;
-        for(int i=1;i<=Ecnt; i++) {
+        for(int i=1;i<=Ecnt;i++) {
             int u, v;
             cin >> u >> v;
-            v[u].push_back(v);            
+            A[u].push_back(v);            
         }
-
+        for(int i=1;i<=Vcnt;i++) {
+            if(checkBi(i) == false) {
+                bi = false;
+                break;
+            }
+        }
+        fill_n(check, MAX, 0);
         bi == true ? cout << "YES" << endl : cout << "NO" << endl;
     }
 }
