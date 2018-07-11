71d368f627fc5dd66595068b9036c7cea0152d40
EdgarHan <poseidon0288@gmail.com>
2018-07-11 17:57:32 +0900

    1707 풀다맘

--- june/13ConnectedComponent/1707.cpp
+++ june/13ConnectedComponent/1707.cpp
@@ -1,25 +1,35 @@
 #include <iostream>
-
+#include <vector>
+#include <stack>
 #define MAX 20001
 
-int check[MAX][2] = {0,};
+int check[MAX] = {0,};
+vector<int> v[MAX];
 using namespace std;
 
+bool checkBi(int s) {
+    check[s] = 1;
+
+    for(int i=0;i<=v[s].size();i++){
+        if(check[v[s][i]] == 1)
+            return false;
+    }
+    return true;
+}
+
 int main() {
-    int K,V,E;
+    int K,Vcnt,Ecnt;
     bool bi;
     cin >> K;
 
     for(int j=1;j<=K;j++) {
-        cin >> V >> E;
+        cin >> Vcnt >> Ecnt;
         bi = true;
-        for(int i=1;i<=E; i++) {
-            int u,v;
+        for(int i=1;i<=Ecnt; i++) {
+            int u, v;
             cin >> u >> v;
-            check[u] = 1;
-            check[v] = 2;
+            v[u].push_back(v);            
         }
-
 
         bi == true ? cout << "YES" << endl : cout << "NO" << endl;
     }
