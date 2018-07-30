3aa4d33f233342179a8ff7789e9b5533f33eb938
EdgarHan <poseidon0288@gmail.com>
2018-07-30 22:40:23 +0900

    18

--- june/13ConnectedComponent/2331.cpp
+++ june/13ConnectedComponent/2331.cpp
@@ -1,36 +1,50 @@
 #include <iostream>
 #include <math.h>
-#include <stack>
+#include <vector>
 
 using namespace std;
 
-int check[10000000] = {0,};
-stack<int> s;
-
+int check[1000000] = {0,};
+int Arr[1000000] = {0,};
+vector<int> v;
+//와 전혀 생각지도 못한 기초연산에서 틀려버림
 int main() {
     string A;
     int P;
     int sum = 0;
     cin >> A >> P;
-    s.push(stoi(A));
+    v.push_back(stoi(A));
     check[stoi(A)] = 1;
     while(true) {
+        //틀렸던 부분임 하..
         for(int i=0;i<A.length();i++) {
-            int a = A[i] - '0';
-            for(int j=1;j<P;j++) {
-                sum = sum + (a*a);
+            int a = 1;
+            for(int j=0;j<P;j++) {
+                a *= A[i] - '0';
             }
+            sum += a;
         }
         A = to_string(sum);
-        check[sum] += 1;  
-        if(check[sum] > 2) { break; }
-        s.push(sum);
+        check[sum] += 1;
+        if(check[sum] > 1) {
+            break;
+        }
+        Arr[v.back()] = sum;
+        v.push_back(sum);
         sum = 0;
     }
-    while(!s.empty()) {
-        if(check[s.top()] == 1) { break; }
-        s.pop();
+    
+    int e = sum;
+    int cnt = 0;
+    
+    while(true) {
+        if(Arr[e] == 0) {
+            break;
+        }
+        e = Arr[e];
+        cnt ++;
     }
-    cout << s.size() << endl;
+
+    cout << v.size() - cnt - 1<< endl;
     return 0;
 }
