a121b32abb99f3d09f1c49e6abb981718f5cf2e8
EdgarHan <poseidon0288@gmail.com>
2018-08-05 23:58:24 +0900

    메모리초과 왜

--- june/13ConnectedComponent/7576.cpp
+++ june/13ConnectedComponent/7576.cpp
@@ -1,3 +1,4 @@
+//입력에서 알아서 띄어쓰기가 들어오면 %1d 쓰지말것 (-1 이 1로인식)
 #include <iostream>
 #include <queue>
 
@@ -21,13 +22,14 @@ bool isArea(int y, int x){
 
 int bfs(int y, int x) {
     int distance = 2;
+    int maxCnt = 0;
     //일단 시작성공
     map[y][x] = distance;
     queue<pair<int, int>> q;
     q.push(pair<int, int>(y, x));
+    pair<int,int> p;
 
     while(!q.empty()) {
-        pair<int,int> p;
         int qy = p.first = q.front().first;
         int qx = p.second = q.front().second;
         distance++;
@@ -35,11 +37,11 @@ int bfs(int y, int x) {
 
         for(int i=0;i<4;i++) {
             int dy = qy + dir[i][0];
-            int dx = qx - dir[i][1];
+            int dx = qx + dir[i][1];
             
-            if(isArea(dy,dx) && !(map[dy][dx] == -1)) {
-                if(map[dy][dx] == 0 || (map[dy][dx] > map[qy][qx])) {
-                    map[dy][dx] = min(map[qy][qx]+1, distance);
+            if(isArea(dy,dx)) {
+                if(map[dy][dx] == 0 || !(map[dy][dx] == -1) && (map[dy][dx] > map[qy][qx])) {
+                    maxCnt = map[dy][dx] = min(map[qy][qx]+1, distance);
                     q.push(pair<int,int>(dy, dx));
                 }
             }
@@ -47,25 +49,38 @@ int bfs(int y, int x) {
         }
     }
 
-    return 0;
+    return maxCnt;
 }
 
-int getSolution() {
+void getSolution() {
+    int maxCnt = 0;
+    int val = 0;
     for(int i=0;i<N;i++) {
         for(int j=0;j<M;j++) {
             if(map[i][j] == 1) {
-                bfs(i, j);
+                val = bfs(i, j);
+                maxCnt = maxCnt == 0 ? val : min(val, maxCnt);
             }
         }
     }
 
+    // 디버깅용
+    // for(int i=0;i<N;i++) {
+    //     for(int j=0;j<M;j++){
+    //         printf("%d ", map[i][j]);
+    //     }
+    //     printf("\n");
+    // }
     for(int i=0;i<N;i++) {
-        for(int j=0;j<M;j++){
-            printf("%d ", map[i][j]);
+        for(int j=0;j<M;j++) {
+            if(map[i][j] == 0) {
+                printf("%d",-1);
+                return;
+            }
         }
-        printf("\n");
     }
-
+    // 시작점이 원래 0 일로쳐야하는데 구분을 위해 2부터시작해서
+    maxCnt == 0 ? printf("%d", 0) : printf("%d", maxCnt-2);
 }
 
 int main() {
@@ -73,7 +88,7 @@ int main() {
 
     for(int i=0;i<N;i++) {
         for(int j=0;j<M;j++){
-            scanf("%1d", &map[i][j]);
+            scanf("%d", &map[i][j]);
         }
     }
 
